e68998c57e6cc4ddb5b3b5424b5e5268574b926b
lin62 <asterlin7890@gmail.com>
2025-04-10 14:35:54 -0400

    Add the Trail Renderer

--- Assets/Scripts/FollowTheMouse.cs
+++ Assets/Scripts/FollowTheMouse.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 
 public class FollowTheMouse : MonoBehaviour
 {
+    SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     private void Update()
     {
         //Vector2 mousPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         //transform.position = mousPos;
 
         transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            sr.color = Random.ColorHSV();
+        }
     }
 }
